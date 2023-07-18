f0850e4d739d9381ff5e9d3577e4c1364b1ef256
Mery9yuka <benhaida.maryem@gmail.com>
2023-07-18 15:18:51 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,12 @@
  *
  */
 void print_alphabet(void)
-
-        int 'i';
-        for('i'=97; 'i'<=122; 'i'++)
 {
-	_putchar('i');
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
 	_putchar('\n');
 }
