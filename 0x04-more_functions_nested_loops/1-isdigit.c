86552f7652c5dacaec2e6f8e5142c33188890591
Mery9yuka <benhaida.maryem@gmail.com>
2023-07-20 19:28:28 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
  * _isdigit - checks for a digit
- *
  * @c: input to b checked
- *
  * Return: 1 if it is a digit, 0 otherwise
 */
 int _isdigit(int c)
 {
-	for (c >= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
