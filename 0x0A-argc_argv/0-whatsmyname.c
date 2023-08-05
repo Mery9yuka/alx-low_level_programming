49be30ce2992db06c5388835ba0dab00637d1c00
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-04 19:35:32 -0700

    argv, argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,7 @@
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,10 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);
-int main(int __attribute__((unused)) argc, char *argv[]);
-
-
-
-
 
 #endif
