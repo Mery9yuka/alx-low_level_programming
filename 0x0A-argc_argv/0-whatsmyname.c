26f94eed74ad1c1e5dd7abcc6fdbc65463fc349e
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-04 19:31:23 -0700

    argv, argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: int
+ * @argv: list
+ *
+ * Return: Always 0 (success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
