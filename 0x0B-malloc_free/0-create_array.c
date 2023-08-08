b50aeaebf00380b067dddb9020cde17b297348dd
root <root@DESKTOP-327RP0B.localdomain>
2023-08-08 01:45:08 +0100

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *create_array - function that creates an array of chars
+ * @size: size of array
+ * @c: char to init
+ * Return: a pointer to array initialized or NULL
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *i = malloc(size);
+
+	if (size == 0 || i == 0)
+		return (0);
+
+	while (size--)
+		i[size] = c;
+	
+	return (i);
+}
