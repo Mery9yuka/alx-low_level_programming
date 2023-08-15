9c0ab333c05470b7c307a05e3d54446a14bd0e8f
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-14 18:37:50 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOC_H
+
+/**
+ * struct dog - dog struct
+ * @name: name of a dog
+ * @age: age of dog
+ * @owner: dog's owner's name
+ * description: dog struct description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
