1a2040d330fd8bb6e002046b6bac6596c5dfb996
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-17 15:34:29 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,18 +4,17 @@
  * @n: nb of arguments
  * @...: int of sum
  * Return: 0 if n == 0
-*/
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	int a = 0, i = n;
 	va_list ap;
 
-	if(!n)
+	if (!n)
 		return (0);
 	va_start(ap, n);
-	while(i--)
+	while (i--)
 		a += va_arg(ap, int);
 	va_end(ap);
 	return (a);
-		
 }
