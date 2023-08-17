60e407e19520a0c0a7c6d38af6427d5541327158
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-16 19:52:33 -0700

    function p

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,10 +19,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-		{
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (NULL);
