f18ced8cca9f1da9d40e8be9be015f25b143f907
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-29 14:34:30 -0700

    print all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,13 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t nd = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
+		nd++;
 	}
-	return (nodes);
+	return (nd);
 }
