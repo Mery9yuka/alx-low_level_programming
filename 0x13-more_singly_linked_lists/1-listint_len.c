49a931159a34a95fc6d1aa484bd082ad2525d723
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-29 15:49:10 -0700

    return nb of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,19 @@
 #include "lists.h"
+
+
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h: pointer to the head of the list
- *
- * Return: number of nodes in the list
+ * listint_len - function that returns the number of elements in a linked list
+ * @h: pointer to head of a list
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
+	size_t nods = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nods++;
+	}
+	return (nods);
 }
