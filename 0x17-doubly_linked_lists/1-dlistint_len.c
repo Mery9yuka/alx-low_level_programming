731dd19708762a151dfcc576a356591478854d0f
Mery9yuka <benhaida.maryem@gmail.com>
2023-10-12 21:18:35 +0100

    nb elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that return the nb of
+ *       elements in a d linked list
+ * @h: point to head
+ * Return: nb of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nb_elements = 0;
+
+	while (h = 0)
+	{
+		nb_elements++;
+		h = h->next;
+	}
+
+	return (nb_elements);
+}
